fe4da737dcf78a3ec3cfd5da352ad03c317c798c
Alessandro Barbieri <info@alessandrobarbieri.net>
2016-06-09 15:41:48 +0200

    Add "extra" feature and optimized namespaces

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# gulpfile
-gulpfile
+### GULPFILE ###
+
+This gulpfile has been created to split `app` files from the `admin` ones. It has also an `extra` feature that let's you create the namespace you want without modifing the Gulpfile settings.
+
+It has been thought for Ruby on Rails 4.2.6 or later and Node 6.2.0 or later.
+
+# Required structure #
+
+In order to work you need to have the following directory structure:
+
+**JAVASCRIPT**
+```
+  app/
+    assets/
+      javascripts/
+
+        application.js    -> Rails pipeline referral
+        app/              -> Put here any App js file
+          app.js          -> Gulp (browserify) referral
+
+        dist/
+          app.bundle.js   -> File to require in App Rails pipeline
+```
+
+**STYLESHEETS**
+```
+  app/
+    assets/
+      stylesheets/
+
+        application.css   -> Rails pipeline referral
+        app/              -> Put here any App css file
+          app.scss        -> Gulp referral
+
+        dist/
+          app.css         -> File to require in App Rails pipeline
+```
+
+You can use the following commands:
+
+* `gulp sass`: preprocesses sass files
+* `gulp js`: preprocesses js files
+* `gulp`: executes `gulp sass` and `gulp js`
+* `gulp watch`: watches stylesheet and javascript directories and executes `gulp sass` and `gulp js` on every file change
+* `gulp bs`: runs Browsersync in background and executes `gulp sass` and `gulp js` on every file change
+
+# Admin files #
+
+If you want to keep your admin files separated you can create the same structure as before but within `admin` namespace. Refer to the following schema.
+
+**JAVASCRIPT**
+```
+  app/
+    assets/
+      javascripts/
+
+        admin.js          -> Rails pipeline referral
+        admin/            -> Put here any Admin js file
+          admin.js        -> Gulp (browserify) referral
+
+        dist/
+          admin.bundle.js -> File to require in Admin Rails pipeline
+```
+
+**STYLESHEETS**
+```
+  app/
+    assets/
+      stylesheets/
+
+        admin.css         -> Rails pipeline referral
+        admin/            -> Put here any Admin css file
+          admin.scss      -> Gulp referral
+
+        dist/
+          admin.css       -> File to require in Admin Rails pipeline
+```
+
+In order to refer to `admin` namespace just add `--admin` at the end of the gulp command. For example `gulp watch --admin` or  simply `gulp --admin`.
+
+# Extra feature #
+
+You might want to have third namespace for a plugin that you use only on one specific view of your app. For example a script called `myPlugin`. The only thing you have to do is create the structure for it as you have done for admin and replacing admin with `myPlugin`. Then you'll be able to execute gulp commands using appendin `--extra myPlugin` to them. For example `gulp watch --extra myPlugin` or `gulp --extra myPlugin`.
+
+# NEXT FEATURES #
+* Implement a command to create the structure automagically
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,35 @@
+/* "esversion: 6" */
+
 'use strict';
 
 /*
 | - - - - - - - - - - - - - - - - - - - - 
 | @ GULP File
 | @ Github: giuseppesalvo
+| @ Github: a-barbieri
 | - - - - - - - - - - - - - - - - - - - -
 */
 
 //Dependencies
-const gulp          = require('gulp' )
-    , browserify    = require('browserify')
-    , sass          = require('gulp-sass' )
-    , autoprefixer  = require('gulp-autoprefixer' )
-    , gp_notify     = require("gulp-notify" )
-    , gutil         = require('gulp-util' )
-    , browserSync   = require('browser-sync').create()
-    , fs            = require('fs')
-    , uglify        = require('gulp-uglify')
-    , argv          = require('yargs').argv
-
+const gulp            = require('gulp' ), 
+        browserify    = require('browserify'), 
+        sass          = require('gulp-sass' ), 
+        autoprefixer  = require('gulp-autoprefixer' ), 
+        gp_notify     = require("gulp-notify" ), 
+        gutil         = require('gulp-util' ), 
+        browserSync   = require('browser-sync').create(), 
+        fs            = require('fs'), 
+        uglify        = require('gulp-uglify'), 
+        argv          = require('yargs').argv,
+        expect        = require('gulp-expect-file');
 
 /**
  * Utils
  */
-const errorLog = err  => gp_notify({ message: err, sound: true, onLast: false } ).write( err );
-const notify   = msg  => gp_notify({ message: msg, onLast: true } );
-const log      = msg  => gutil.log( gutil.colors.blue( msg ) );
-const assets   = path => './app/assets' + path ;
+const errorLog = (err)  => gp_notify( { message: err, sound: true, onLast: false } ).write( err );
+const notify   = (msg)  => gp_notify( { message: msg, onLast: true } );
+const log      = (msg)  => gutil.log( gutil.colors.blue( msg ) );
+const assets   = (path) => 'app/assets' + path ;
 
 /**
  * Big class
@@ -34,16 +37,31 @@ const assets   = path => './app/assets' + path ;
 
 class Gulp {
 
-    constructor({ paths, check, dests, proxy = "http://localhost:3000" }) {
+    constructor( proxy = "http://localhost:3000" ) {
 
+        if ( argv.admin )
+            var NS = "admin";
+        else if ( argv.extra )
+            var NS = argv.extra
+        else
+            var NS = "app";
+
+        this.paths = { sass: {}, js: {} }
+        this.check = { sass: {}, js: {}, browserify: {} }
+        this.dests = { sass: {}, js: {} } 
+
+        this.paths.sass[ NS ]       = assets(`/stylesheets/${NS}/${NS}.scss`);
+        this.paths.js[ NS ]         = assets(`/javascripts/${NS}/${NS}.js`);
+        this.check.sass[ NS ]       = assets(`/stylesheets/${NS}/**/*.scss`);
+        this.check.js[ NS ]         = assets(`/javascripts/${NS}/**/*.js`);
+        this.check.browserify[ NS ] = assets(`/javascripts/${NS}/**/*.js`);
+        this.dests.sass[ NS ]       = assets('/stylesheets/dist');
+        this.dests.js[ NS ]         = assets(`/javascripts/dist/${NS}.bundle.js`);
+
+        this.namespace = NS;
         this.proxy = proxy;
-        this.paths = paths;
-        this.check = check;
-        this.dests = dests;
 
-        this.namespace = argv.admin ? "admin" : "app" ;
-
-        this.tasks();    
+        this.tasks();
     }
 
     tasks() {
@@ -65,23 +83,26 @@ class Gulp {
         const prefix_conf = {
             browsers : ['> 1%', 'IE 7'],
             cascade  : false
-        }
+        };
 
         gulp.src( this.paths.sass[this.namespace] )
+            .pipe( expect( { errorOnFailure: true }, this.paths.sass[this.namespace] ).on( 'error', errorLog ) )
             .pipe( sass().on( 'error', errorLog ) )
             .pipe( autoprefixer(prefix_conf) )
             .pipe( gulp.dest( this.dests.sass[this.namespace] ) )
             .pipe( browserSync.stream() )
             .pipe( notify( "Sass compiled" ) )
-
+            .pipe( notify( function(file) {
+                return "Some file: " + file.relative;
+            }));
     }
 
     js() {
 
-        const extensions = [ ".js", ".jsx", ".vertex", ".fragment" ]
-            , paths      = [ './node_modules', this.check.browserify[this.namespace] ]
-            , presets    = [ "es2015", "react" ]
-            , stringExt  = [ ".vertex", ".fragment" ]
+        const extensions = [ ".js", ".jsx", ".vertex", ".fragment" ], 
+                paths      = [ './node_modules', this.check.browserify[this.namespace] ], 
+                presets    = [ "es2015", "react" ], 
+                stringExt  = [ ".vertex", ".fragment" ];
 
         const br = browserify( this.paths.js[this.namespace], { extensions, paths });
 
@@ -91,27 +112,28 @@ class Gulp {
           .on( 'error', errorLog )
           .pipe( notify( "Scripts compiled" ) )
           .pipe( browserSync.stream() )
-          .pipe( fs.createWriteStream( this.dests.js[this.namespace] ) )
+          .pipe( fs.createWriteStream( this.dests.js[this.namespace] ) );
     }
     
     uglify() {
 
         let dest = this.dests.js[this.namespace].split('/');
             dest.splice(-1,1);
-            dest = dest.join('/')
+            dest = dest.join('/');
     
         gulp.src( this.dests.js[this.namespace] )
             .pipe( uglify() )
             .pipe( gulp.dest( dest + "/min" ) )
-            .pipe( notify('uglify') )
+            .pipe( notify('uglify') );
     }
 
     browserSync( settings = {} ) {
         settings.proxy = this.proxy;
-        browserSync.init( settings )
+        browserSync.init( settings );
     }
 
     watch() {
+
         gulp.watch( this.check.sass[this.namespace] , ['sass']).on( 'change', browserSync.reload );
         gulp.watch( this.check.js[this.namespace]   , ['js'  ]).on( 'change', browserSync.reload );
     }
@@ -120,43 +142,5 @@ class Gulp {
 /**
  * Init
  */
-new Gulp({
 
-    paths: {
-        sass : {
-            app   : assets('/stylesheets/app/app.scss'),
-            admin : assets('/stylesheets/admin/admin.scss'),
-        },
-        js : {
-            app   : assets('/javascripts/app/app.js'),
-            admin : assets('/javascripts/admin/admin.js')
-        }
-    },
-
-    // avoid dests watch
-    check : {
-        sass : {
-            app   : assets('/stylesheets/app/**/*.scss'),
-            admin : assets('/stylesheets/admin/**/*.scss'),
-        },
-        js : {
-            app   : assets('/javascripts/app/**/*.js'),
-            admin : assets('/javascripts/admin/**/*.js'),
-        },
-        browserify : {
-            app   : assets('/javascripts/app/**/*.js'),
-            admin : assets('/javascripts/admin/**/*.js')
-        }
-    },
-    
-    dests : {
-        sass: {
-            app   : assets('/stylesheets/dist'),
-            admin : assets('/stylesheets/dist'),
-        },
-        js: {
-            app   : assets('/javascripts/dist/app.bundle.js'),
-            admin : assets('/javascripts/dist/admin.bundle.js')
-        }
-    }
-})
+new Gulp();
